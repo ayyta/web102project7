07e9d72d52ddff48649a73908304583975ba4aa4
ALVIS YONG <yong.mark.alvis@gmail.com>
2023-10-29 18:54:48 -0700

    Added skeleton for all pages, working on css and styling

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <div className='left-container'>
+        <div>Home</div>
+        <div>Create a Crewmate!</div>
+        <div>Crewmate Gallery</div>
+      </div>
+      <div className='right-container'>blahblah</div>
+    </main>
+  )
+}
+
+export default App
